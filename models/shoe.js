0f618c80f66540a9e3ac7f56017e6b134feb7eb9
Sushma-362 <sushma.edu2023@gmail.com>
2024-04-17 01:03:53 -0500

    123

--- models/shoe.js
+++ models/shoe.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose")
 const shoeSchema = mongoose.Schema({
 shoe_type: String,
-shoe_brand: String,
-cost: Number
+shoe_brand: {
+    type: String,
+    minlength: 1,
+    maxlength: 10
+},
+cost: {
+    type:Number,
+    min : 1,
+    max : 362,
+}
 })
 module.exports = mongoose.model("shoe",
 shoeSchema)
--- routes/shoe.js
+++ routes/shoe.js
@@ -1,10 +1,26 @@
 var express = require('express');
+var passport = require('passport');
 const shoe_controlers= require('../controllers/shoe');
 var router = express.Router();
 /* GET costumes */
 router.get('/', shoe_controlers.shoe_view_all_Page );
 router.get('/detail', shoe_controlers.shoe_view_one_Page);
-router.get('/create', shoe_controlers.shoe_create_Page);
-router.get('/update', shoe_controlers.shoe_update_Page);
-router.get('/delete', shoe_controlers.shoe_delete_Page);
+//router.get('/create', shoe_controlers.shoe_create_Page);
+//router.get('/update', shoe_controlers.shoe_update_Page);
+//router.get('/delete', shoe_controlers.shoe_delete_Page);
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+router.get('/create',secured, shoe_controlers.shoe_create_Page);
+router.get('/update',secured, shoe_controlers.shoe_update_Page);
+router.get('/delete',secured, shoe_controlers.shoe_delete_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    }); 
 module.exports = router;
